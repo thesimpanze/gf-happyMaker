bfc9b1b14c71353af53e7f16de0c3272997d643f
Rasyid Nuruddin <rasyid8nuruddin@gmail.com>
2026-01-14 17:49:36 +0700

    Add: HoverMe component for tooltip display
    Introduces a new HoverMe React component that shows a tooltip with custom text near the mouse cursor when hovering over its children.

---
+++ src/components/hover.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const HoverMe = ({children, text}) => {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [show, setShow] = useState(false);
+
+  return (
+    <div
+      className=""
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+      onMouseMove={(e) => setPos({ x: e.clientX + 1, y: e.clientY + 12 })}
+    >
+      {children}
+      {show && (
+        <div
+          className="fixed z-50 pointer-events-none
+                     bg-white text-primary text-sm px-3 py-1 rounded-md"
+          style={{ left: pos.x, top: pos.y }}
+        >
+         {text}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HoverMe;
